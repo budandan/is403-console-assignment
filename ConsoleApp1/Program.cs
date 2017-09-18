3374e41b9a1499950198edddb245e1bba3680b6a
budandan <danielrferolino@gmail.com>
2017-09-18 15:18:11 -0600

    Adds classes and logic. Needs try-catch statement for int expr

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
@@ -10,7 +11,56 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hey Wasdfasdf");
+            int numberOfTeams;
+            Console.Write("How many teams? ");
+            numberOfTeams = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            // initialize List of teams with size numberOfTeams
+            List<SoccerTeam> soccerTeams = new List<SoccerTeam>();
+
+            // ask for team names
+            string name; // placeholder var for input
+            int points; // placebolder var for points
+            for (int i = 0; i < numberOfTeams; i++)
+            {
+                Console.Write("Enter Team " + (i + 1) + "\'s name: ");
+                name = UppercaseFirst(Console.ReadLine());
+                soccerTeams.Add(new SoccerTeam());
+                soccerTeams[i].name = name;
+                Console.WriteLine();
+                Console.Write("Enter Team " + soccerTeams[i].name + "\'s points: ");
+                points = Convert.ToInt32(Console.ReadLine());
+                soccerTeams[i].points = points;
+                Console.WriteLine("\n");
+            }
+
+            // sort list desc by points
+            List<SoccerTeam> sortedTeams = soccerTeams.OrderByDescending(team => team.points).ToList();
+
+            // display formatted data
+            Console.WriteLine("Position\t\tName\t\tPoints");
+            Console.WriteLine("--------\t\t----\t\t------");
+            for (int i = 0; i < numberOfTeams; i++)
+            {
+                Console.WriteLine((i + 1) + "       \t\t" + sortedTeams[i].name + "\t\t" + sortedTeams[i].points);
+            }
+
+            // End programs
+            Console.WriteLine("\n\nPress any key to continue . . .");
+            Console.ReadKey();
+        }
+
+        // given function to make first letter uppercase
+        static string UppercaseFirst(string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            // Return char and concat substring.
+            return char.ToUpper(s[0]) + s.Substring(1);
         }
     }
 }
---
+++ ConsoleApp1/SoccerTeam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SoccerTeam : Team
+    {
+        public SoccerTeam() {}
+        public int draw;
+        public int goalsFor;
+        public int goalsAgainst;
+        public int differential;
+        public int points;
+    }
+
+
+}
